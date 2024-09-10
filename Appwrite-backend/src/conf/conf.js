13ea73141d5ddcf008b0a6c40dd7239d1154e362
Bharat <kadamb208@gmail.com>
2024-09-10 09:15:32 +0530

    .env files add

--- Appwrite-backend/src/conf/conf.js
+++ Appwrite-backend/src/conf/conf.js
@@ -5,4 +5,4 @@ VITE_APPWRITE_DATABASE_ID:String(import.meta.env.VITE_APPWRITE_DATABASE_ID)
 VITE_APPWRITE_COLLECTION_ID:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID)
 VITE_APPWRITE_BUCKET_ID:String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 
-export default conf  
+export default conf   
