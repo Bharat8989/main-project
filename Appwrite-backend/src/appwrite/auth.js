2bbec93e0ce0466ea0981240e3ef64e581261942
Bharat <kadamb208@gmail.com>
2024-09-11 13:46:16 +0530

    same changes

--- Appwrite-backend/src/appwrite/auth.js
+++ Appwrite-backend/src/appwrite/auth.js
@@ -1,5 +1,17 @@
 import conf from "../conf/conf.js";
 import {Client,Account, ID} from 'appwrite'
 
-export class AuthService {}
+export class AuthService {
+    client =new Client();
+    account;
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        // ssrExportAllKey(conf.projectId)
+        this.account=new Account(this.client)
+    }
+}
+
+const AuthService =new AuthService();
 export default AuthService;  
