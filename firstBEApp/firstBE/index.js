c48eaecdbbec080f3f6249ec36757c1f72cf8f8d
Bharat <kadamb208@gmail.com>
2024-12-07 14:10:36 +0530

    middlewate

---
+++ firstBEApp/firstBE/index.js
@@ -0,0 +1,24 @@
+// including express module and initialising an app 
+const express=require('express');
+const app =express();
+
+app.get('/', (req, res) => {
+    console.log("get request receive");
+    res.send('Hello Bharat!')
+  })
+
+
+//variable that stores the port number
+
+const port =3001;
+
+//  request ->get /put /post delete /
+//path - /,/about  /block
+
+//start your app or server
+app.listen(port,()=>{
+    console.log("applications start");
+});
+
+
+// middleware 
