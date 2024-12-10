77825cdb0fe0a30820bb8a5bf77b59be0a26b47c
Bharat <kadamb208@gmail.com>
2024-12-10 09:54:01 +0530

    middleware logging

--- firstBEApp/Middleware/index.js
+++ firstBEApp/Middleware/index.js
@@ -7,7 +7,31 @@ const port = 3000
 // express.json()
 app.use(express.json());
 
+// middleware - logging ,auth, s validations
+
+const loggingMiddleware=function (req,res,next){
+    console.log("logging krra hu")
+    next();
+
+}
+app.use(loggingMiddleware);
+const authMiddleware=function (req,res,next){
+    console.log("auth krra hu")
+    next();
+
+}
+app.use(authMiddleware);
+const validationMiddleware=function (req,res,next){
+    console.log("validations krra hu")
+    next();
+
+}
+app.use(validationMiddleware);
+
+
+
 app.get('/', (req, res) => {
+  consolo
     console.log(req.body);
   res.send('Hello World!')
 })
