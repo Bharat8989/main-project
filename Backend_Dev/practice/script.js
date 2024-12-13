64a7688a68ac7f950455bf7fe5bcd2f45a285ab6
Bharat <kadamb208@gmail.com>
2024-12-13 18:53:54 +0530

    npm understanding

---
+++ Backend_Dev/practice/script.js
@@ -0,0 +1,28 @@
+// const fs= require('fs');
+
+// // write files 
+
+// fs.writeFile('hey.tex','hello , how can i help you',function(err){
+//     if(err) console.error(err);
+//         else console.log("done ");
+// });
+// // appendFiles
+
+// fs.appendFile('hey.tex', ' do you no',function(err){
+//     if(err) console.error(err);
+//         else console.log("append files");
+// })
+
+// // rename 
+// fs.rename('hey.tex','hello.txt',function(err){
+//     if(err) console.error(err);
+//     else console.log("Done ");
+// })
+
+const http=require('http');
+const server =http.createServer(function(req,res){
+    res.end("hello world");
+
+})
+server.listen(3000);
+
