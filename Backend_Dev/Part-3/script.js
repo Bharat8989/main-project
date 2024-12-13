39ea8129e9d22a5f2782ece27c07583682da633f
Bharat <kadamb208@gmail.com>
2024-12-13 18:53:54 +0530

    npm understanding

---
+++ Backend_Dev/Part-3/script.js
@@ -0,0 +1,14 @@
+/**  npm understanding 
+ * installing and uninstalling anything basics and advanced 
+ * understanding node modules 
+ * dependencies
+ * devdependencies
+ * scripts understanding default scripts Path and custom scripts
+ * yh
+ */
+
+const http=require('http');
+const server=http.createServer(function(req,res){
+    res.end("hello pavan!");
+})
+server.listen(3000);
