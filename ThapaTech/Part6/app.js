9728837c69719e6adadeeed1b4cfd4d92c7a4456
Bharat <kadamb208@gmail.com>
2024-12-16 14:44:18 +0530

    global and globalThis

---
+++ ThapaTech/Part6/app.js
@@ -0,0 +1,7 @@
+// console.log(global);
+
+global.console.log("hello world");
+// globalThis.console.log(module);
+// console.log(globalThis.process);
+// console.log(window.document); //js 
+console.log(globalThis.module);
