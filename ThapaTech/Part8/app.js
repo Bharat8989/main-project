bbc19f3b1b8bf2d2c3fdba3b153a2e72ff72c29e
Bharat <kadamb208@gmail.com>
2024-12-17 14:24:24 +0530

    Name exports

---
+++ ThapaTech/Part8/app.js
@@ -0,0 +1,10 @@
+
+// const add=require('./math');
+// const {add,sub,div,PI}=require('./math');
+
+const math=require('./math');
+
+console.log(math.add(3,4));
+console.log(math.sub(45,34));
+console.log(math.div(45,5));
+console.log(math.PI);
