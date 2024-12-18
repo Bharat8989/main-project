a4173fa150a5a384e69f91cab723a60525e3dd6c
Bharat <kadamb208@gmail.com>
2024-12-18 18:53:58 +0530

    EJS and Dynamic routing

---
+++ Backend_Dev/One_to_9/Part-5/Part4.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const app = express();
+
+
+
+// // Middleware 1
+// app.use(function (req, res, next) {
+//     console.log('Middleware executed');
+//     next();
+// });
+
+// // Middleware 2
+// app.use(function (req, res, next) {
+//     console.log('Middleware executed again');
+//     next();
+// });
+
+//error handling sessions and cookies 
+//blob
+app.use(express.json());
+
+app.use(express.urlencoded({ extended:true}));
+
+
+
+// Route: Home
+app.get('/', function (req, res) {
+    res.send('Champion mera pavan');
+});
+
+// Route: About
+app.get('/about', function (req, res) {
+    res.send('About page loading');
+});
+
+// Route: Photo (simulating an error)
+app.get('/photo', function (req, res, next) {
+    next(new Error('Something went wrong'));
+});
+
+// 
+app.use(function (err, req, res, next) {
+    console.error(err.stack); // Corrected typo here
+    res.status(500).send("Something went wrong, we don't have any idea");
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
