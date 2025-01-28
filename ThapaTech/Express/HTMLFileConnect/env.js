675fbb2670bcaef7f94f5de8fb31e116b01b0f48
Bharat <kadamb208@gmail.com>
2025-01-28 15:39:50 +0530

    learing express

---
+++ ThapaTech/Express/HTMLFileConnect/env.js
@@ -0,0 +1,11 @@
+// env.js
+const z = require('zod');
+
+// Define the portSchema using Zod to validate the port number
+const portSchema = z.coerce.number().min(1).max(6500).default(3000);
+
+// Parse and validate process.env.PORT, and fall back to default if invalid
+const PORT = portSchema.parse(process.env.PORT);
+
+module.exports = { PORT };
+ 
