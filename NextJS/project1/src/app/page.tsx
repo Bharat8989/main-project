843866c3fd60e5ff575b64ef4661a8a4c5ab0e44
Bharat <kadamb208@gmail.com>
2025-02-07 19:05:04 +0530

    nextjs project start

---
+++ NextJS/project1/src/app/page.tsx
@@ -0,0 +1,27 @@
+
+// export default function Home() {
+//   return (
+//     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+//      <h1>hello</h1>
+//     </div>
+//   );
+// }
+
+
+import dbConnect from "../lib/dbConnect";
+
+export default async function Home() {
+  await dbConnect(); // Ensure database connection
+
+  return (
+    <div
+      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"
+      style={{ fontFamily: "var(--font-geist-sans)" }}
+    >
+      <h1>Hello</h1>
+      <p>Connected to Database</p>
+    </div>
+    
+  );
+}
+ 
