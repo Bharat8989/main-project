b1af22c119cc635cd60e23e5193bb676de27b249
Bharat <kadamb208@gmail.com>
2025-03-21 08:51:29 +0530

    remove the files for the sendmain

--- NextJS/stackflow-appwrite/src/utils/slugify.ts
+++
@@ -1,9 +0,0 @@
-export default function slugify(text: string) {
-  return text
-      .toString()
-      .toLowerCase()
-      .trim() // Trim whitespace from both sides of the string
-      .replace(/\s+/g, "-") // Replace spaces with a dash
-      .replace(/[^\w\-]+/g, "") // Remove all non-word characters
-      .replace(/\-\-+/g, "-"); // Replace multiple dashes with a single dash
-}
