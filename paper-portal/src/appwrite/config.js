45874feca522367e798a5e37e775aa2b05f437b7
Bharat <kadamb208@gmail.com>
2026-02-03 23:26:10 +0530

    paper portal

--- paper-portal/src/appwrite/config.js
+++
@@ -1,9 +0,0 @@
-import { Client } from "appwrite";
-
-const client = new Client();
-
-client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
-
-export default client;
--- paper-portal/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-function Home(){
-  return(
-    <div>
-      <h2 className="text-center text-blue-500" >Welcome to the Home Page</h2>
-    </div>
-  )   
-}
-export default Home;
